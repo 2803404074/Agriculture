31499f48b4bbed7822d252aed4439c78366fce72
音乐队长 <2803404074@qq.com>
2019-07-10 10:56:30 +0800

    测试

--- app/src/main/java/com/tzl/agriculture/application/AgricultureApplication.java
+++ app/src/main/java/com/tzl/agriculture/application/AgricultureApplication.java
@@ -5,7 +5,11 @@ import android.app.Application;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.mob.MobSDK;
 
+/**
+ * 牛逼
+ */
 public class AgricultureApplication extends Application {
+
 
     @Override
     public void onCreate() {
